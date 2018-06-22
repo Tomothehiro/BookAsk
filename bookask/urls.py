8b1481c194ad6ece3e40bad179ac05b38e132e3d
tomo <tomonouchi@hotmail.com>
2018-06-22 07:03:52 -0700

    add login form

--- bookask/urls.py
+++ bookask/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^book/', include('book.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
