53866470b55f422c8e45bcc5d608cf9628f859a9
tomo <tomonouchi@hotmail.com>
2018-06-28 00:04:49 -0700

    update model

--- book/admin.py
+++ book/admin.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from .models import Book, Question, Answer
+from .models import Book, Category, Question, Answer, Comment, Like, Watch
 
 admin.site.register(Book)
+admin.site.register(Category)
 admin.site.register(Question)
 admin.site.register(Answer)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Watch)
--- book/models.py
+++ book/models.py
@@ -2,14 +2,23 @@ from django.db import models
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __unicode__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=250)
     author = models.CharField(max_length=250)
-    category = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     cover = models.FileField()
 
     def get_absolute_url(self):
         return reverse('book:book', kwargs={'pk': self.pk})
+
+    def __unicode__(self):
+        return self.title
 
 class Question(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -39,10 +48,19 @@ class Comment(models.Model):
     comment = models.CharField(max_length=4000)
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
 
+    def __unicode__(self):
+        return self.comment
+
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
 
+    def __unicode__(self):
+        return self.book
+
 class Watch(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    
+    def __unicode__(self):
+        return self.book
